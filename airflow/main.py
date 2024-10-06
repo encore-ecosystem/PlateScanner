5a42bec8692a83098aba42d85df0dfc8c7063324
Maksim Shushkevich <meshushkevich@edu.hse.ru>
2024-10-06 13:56:54 +0300

    Update Docker

--- airflow/main.py
+++ airflow/main.py
@@ -1,54 +1,39 @@
-from ultralytics import YOLO, settings
+from ultralytics import YOLO
+from clearml import Task
 from src import DATA_PATH
-# import mlflow
-
-settings.update({"mlflow": False})
-settings.reset()
 
 config = {
     "batch": 8,
     "imgsz": 1280,
-    "epochs": 1,
-    "name": "YOLOv8",
+    "epochs": 20,
+    "name": "YOLOv5",
     "data": [(path / 'data.yaml').resolve() for path in DATA_PATH.iterdir() if '_Y_' in path.name][0],
 }
 
 
 def main():
-    # mlflow.set_tracking_uri(uri="http://localhost:5000")
-    #
-    # # set the experiment id
-    # ## experiment_name="YOLOv8 Basic Usage of MLFlow"
-    # mlflow.set_experiment(experiment_id="0")
-
-    # start run
-    # with mlflow.start_run():
-        # Logging parameters
-        # mlflow.log_param("epochs", config["epochs"])
-        # mlflow.log_param("batch_size", config["batch"])
-        # mlflow.log_param("img_size", config["imgsz"])
-        # mlflow.log_param("data_path", config["data"])
-        # mlflow.log_param("experiment_name", config["name"])
-
-        # Load and train your YOLOv8 model
-    model = YOLO("yolov8n.pt")
-    results = model.train(
-        data=config["data"],
-        epochs=config["epochs"],
-        batch=config["batch"],
-        name=config["name"],
-        imgsz=config["imgsz"],
+    # Create a ClearML Task
+    task = Task.init(
+        project_name="Testing Yolo 1",
+        task_name="my yolo task 3"
     )
-        #
-        # # Metrics
-        # metrics = results.metrics
-        #
-        # # Logging  Metric
-        # for metric, value in metrics.items():
-        #     mlflow.log_metric(metric, value)
-        #
-        # # Logging model
-        # mlflow.pytorch.log_model(model.model, "YOLOv8_model")
+
+    # Load a model
+    model_variant = "yolov5nu"
+    # Log "model_variant" parameter to task
+    task.set_parameter(name="model_variant", value=model_variant)
+
+    # Load the YOLOv8 model
+    model = YOLO(f'{model_variant}.pt')
+
+    # Put all YOLOv8 arguments in a dictionary and pass it to ClearML
+    # When the arguments are later changed in UI, they will be overridden here!
+    args = config
+    task.connect(args)
+
+    # Train the model
+    # If running remotely, the arguments may be overridden by ClearML if they were changed in the UI
+    results = model.train(**args)
 
 
 if __name__ == '__main__':
