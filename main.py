d68755959c7d78ffbaa6a9a7094ae121a99099c3
Maksim Shushkevich <meshushkevich@edu.hse.ru>
2024-10-30 15:42:32 +0300

    Implemented NMS and Area Filter

--- main.py
+++ main.py
@@ -1,37 +1,57 @@
-from ultralytics import YOLO
-from clearml import Task
-from src import DATA_PATH
-
-
-MODEL = 'YOLO11x'
-
-config = {
-    "batch": 2,
-    "imgsz": 1280,
-    "epochs": 50,
-    "name": MODEL,
-    "data": DATA_PATH / 'AUGMerged' / 'YOLO' / 'data.yaml',
-    "save_period": 2
-}
+from src.model import YoloV5
+from pathlib import Path
+from tqdm import tqdm
+import argparse
+import cv2
+import os
 
 
 def main():
-    # Create a ClearML Task
-    task = Task.init(
-        project_name = "PlateScanner",
-        task_name    = f"Testing {MODEL} on augmented dataset"
-    )
+    # Parse args
+    parser = argparse.ArgumentParser(
+        prog='PlateScanner',
+        description='<todo>',
+        epilog='<todo>')
+    parser.add_argument('input_dir')
+    parser.add_argument('output_dir')
+    parser.add_argument('-m', '--model'      , type=str,   default='yolov5nu')
+    parser.add_argument('-c', '--confidence' , type=float, default=0.06)
 
-    model_variant = MODEL.lower()
+    args = parser.parse_args()
 
-    task.set_parameter(name="model_variant", value=model_variant)
+    # Validate and prepare args
+    root = Path().resolve()
 
-    model = YOLO(f'{model_variant}.pt')
+    input_dir = Path(args.input_dir).resolve()
+    assert input_dir.exists() and input_dir.is_dir(), f"Unable to find input directory {input_dir}"
 
-    args = config
-    task.connect(args)
+    output_dir = Path(args.output_dir).resolve()
+    output_dir.mkdir(exist_ok=True, parents=True)
 
-    model.train(**args)
+    model = args.model.lstrip('.pt')
+    assert f"{model.lower()}.pt" in os.listdir(root / 'model'), f"Unable to find model {model}"
+
+    # Computation
+    model = YoloV5(root / 'model' / f"{model}.pt")
+
+
+    for img_path in tqdm(os.listdir(input_dir), desc='Prediction'):
+        results = model.predict(
+            source     = (input_dir / img_path).__str__(),
+            conf       = args.confidence,
+            line_width = None,
+        )
+        image = cv2.imread(str(input_dir / img_path))
+        for box in results:
+            x1, y1, x2, y2, score, class_id = box
+            cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (205, 0, 255), 3)
+            cv2.putText(
+                image,
+                # parse class id from yaml
+                f"Plate: {score:.2f}",
+                (int(x1 - 15), int(y1 - 10)),
+            cv2.FONT_HERSHEY_SIMPLEX, 1, (205, 0, 255), 3, cv2.LINE_AA)
+        cv2.imwrite((output_dir / img_path).__str__(), image)
 
 
 if __name__ == '__main__':
