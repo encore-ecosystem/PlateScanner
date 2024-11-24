1590c6a714eebe84c38415530b1ba8bef881a238
Pavel Lekomtsev <polekomtsev@edu.hse.ru>
2024-11-24 13:54:47 +0300

    Firstly remaked README.md with article

--- src/bbox/bbox_cwh.py
+++ src/bbox/bbox_cwh.py
@@ -1,5 +1,6 @@
 from .abstract import Bbox
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, box
+from src.bbox.bbox_2xy import Bbox_2xy
 
 
 class Bbox_CWH(Bbox):
@@ -11,21 +12,30 @@ class Bbox_CWH(Bbox):
         return self.bbox[2] * self.bbox[3]
 
     def __and__(self, other: 'Bbox_CWH') -> float:
-        poly_a = Polygon([
-            (self.bbox[0] - self.bbox[2] / 2, self.bbox[1] - self.bbox[3] / 2),
-            (self.bbox[0] + self.bbox[2] / 2, self.bbox[1] - self.bbox[3] / 2),
-            (self.bbox[0] + self.bbox[2] / 2, self.bbox[1] + self.bbox[3] / 2),
-            (self.bbox[0] - self.bbox[2] / 2, self.bbox[1] + self.bbox[3] / 2),
-        ])
+        if isinstance(other, Bbox_CWH):
+            poly_a = Polygon([
+                (self.bbox[0] - self.bbox[2] / 2, self.bbox[1] - self.bbox[3] / 2),
+                (self.bbox[0] + self.bbox[2] / 2, self.bbox[1] - self.bbox[3] / 2),
+                (self.bbox[0] + self.bbox[2] / 2, self.bbox[1] + self.bbox[3] / 2),
+                (self.bbox[0] - self.bbox[2] / 2, self.bbox[1] + self.bbox[3] / 2),
+            ])
+            poly_b = Polygon([
+                (other.bbox[0] - other.bbox[2] / 2, other.bbox[1] - other.bbox[3] / 2),
+                (other.bbox[0] + other.bbox[2] / 2, other.bbox[1] - other.bbox[3] / 2),
+                (other.bbox[0] + other.bbox[2] / 2, other.bbox[1] + other.bbox[3] / 2),
+                (other.bbox[0] - other.bbox[2] / 2, other.bbox[1] + other.bbox[3] / 2),
+            ])
+            return poly_a.intersection(poly_b).area
+        elif isinstance(other, Bbox_2xy):
+            poly_a = Polygon([
+                (self.bbox[0] - self.bbox[2] / 2, self.bbox[1] - self.bbox[3] / 2),
+                (self.bbox[0] + self.bbox[2] / 2, self.bbox[1] - self.bbox[3] / 2),
+                (self.bbox[0] + self.bbox[2] / 2, self.bbox[1] + self.bbox[3] / 2),
+                (self.bbox[0] - self.bbox[2] / 2, self.bbox[1] + self.bbox[3] / 2),
+            ])
+            poly_b = box(*other.bbox)
+            return poly_a.intersection(poly_b).area
 
-        poly_b = Polygon([
-            (other.bbox[0] - other.bbox[2] / 2, other.bbox[1] - other.bbox[3] / 2),
-            (other.bbox[0] + other.bbox[2] / 2, other.bbox[1] - other.bbox[3] / 2),
-            (other.bbox[0] + other.bbox[2] / 2, other.bbox[1] + other.bbox[3] / 2),
-            (other.bbox[0] - other.bbox[2] / 2, other.bbox[1] + other.bbox[3] / 2),
-        ])
-
-        return poly_a.intersection(poly_b).area
 
     def get_poly(self) -> list[tuple[float, float]]:
         poly_a = Polygon([
@@ -35,6 +45,7 @@ class Bbox_CWH(Bbox):
             (self.bbox[0] - self.bbox[2] / 2, self.bbox[1] + self.bbox[3] / 2),
         ])
         return list(poly_a.exterior.coords)
+
 
 __all__ = [
     'Bbox_CWH'
